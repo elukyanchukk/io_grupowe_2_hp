79511a40bb0afef642018d6231fe0a246abfd770
Oliwia Dybowska <oliwiaddybowska@gmail.com>
2024-04-17 19:19:32 +0200

    fixed function

--- zadanie_3.py
+++ zadanie_3.py
@@ -1,49 +1,32 @@
-def licz_sume(dane):
+def licz_sume(fundusz):
+    # Przeliczenie knutów na sykle
+    knuty = sum(fundusz.get("knut"))
+    knut_sykl = knuty // 21
+    knuty = knuty % 21
 
-    przelicznik = {
-        "sykl": {"na_galeon": 1 / 17},
-        "knut": {"na_galeon": 1 / (17 * 21), "na_sykl": 1 / 21}
+    # Dodanie knutów do sykli i przeliczenie sykli na galeony 
+    sykle = sum(fundusz.get("sykl")) + knut_sykl
+    sykl_galeon = sykle // 17
+    sykle = sykle % 17
+
+    # Dodanie sykli do galeonów
+    galeony = sum(fundusz.get("galeon")) + sykl_galeon
+
+    return {
+        "galeon": galeony,
+        "sykl": sykle,
+        "knut": knuty
     }
 
-    wynik = {
-        "galeon": 0,
-        "sykl": 0,
-        "knut": 0
-    }
-
-    for klucz, wartosci in dane.items():
-        for i, wartosc in enumerate(wartosci):
-            if klucz == "galeon":
-                wynik["galeon"] += wartosc
-            elif klucz == "sykl":
-                wynik["sykl"] += wartosc
-            elif klucz == "knut":
-                wynik["knut"] += wartosc
-
-    wynik["knut"] = 0
-    wynik["sykl"] += wynik["knut"] *21
-    wynik["galeon"] += wynik["knut"] // (17 * 21)
-    wynik["knut"] %= 17 * 21
-
-    return wynik
-
-# dane = {
-#         "galeon": [1, 3, 5],
-#         "sykl": [18, 20, 10],
-#         "knut": [30, 40, 7]
-#     }
-
-galeon = list(map(int, input("Podaj ilość monet galeon (oddzielone przecinkami): ").split(',')))
-sykl = list(map(int, input("Podaj ilość monet sykl (oddzielone przecinkami): ").split(',')))
-knut = list(map(int, input("Podaj ilość monet knut (oddzielone przecinkami): ").split(',')))
-
-dane = {
-    "galeon": galeon,
-    "sykl": sykl,
-    "knut": knut
+# Przykładowe wejście
+fundusz = {
+    "galeon": [1, 3, 5],
+    "sykl": [18, 20, 10],
+    "knut": [30, 40, 7]
 }
 
-print(licz_sume(dane))
+wynik = licz_sume(fundusz)
+print(wynik)
 
 
 
